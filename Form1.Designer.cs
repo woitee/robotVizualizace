71a00c0ad4a3673046fdcb8ce7940c40899cfb17
woitee <woitee@gmail.com>
2013-09-17 17:54:56 +0200

    Basing tracing of robot, by coordinates

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,73 +28,167 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.maxLatLabel = new System.Windows.Forms.Label();
+            this.minLatLabel = new System.Windows.Forms.Label();
+            this.maxLonLabel = new System.Windows.Forms.Label();
+            this.minLonLabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.rangeTextBox = new System.Windows.Forms.TextBox();
+            this.angleTextBox = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.nextButton = new System.Windows.Forms.Button();
+            this.autoButton = new System.Windows.Forms.Button();
+            this.autoLabel = new System.Windows.Forms.Label();
+            this.autoTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // pictureBox
             // 
-            this.pictureBox1.BackColor = System.Drawing.Color.White;
-            this.pictureBox1.Location = new System.Drawing.Point(80, 30);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(551, 335);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.pictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox.BackColor = System.Drawing.Color.White;
+            this.pictureBox.Location = new System.Drawing.Point(80, 30);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(539, 414);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
             // 
-            // label1
+            // maxLatLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 30);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.maxLatLabel.AutoSize = true;
+            this.maxLatLabel.Location = new System.Drawing.Point(13, 30);
+            this.maxLatLabel.Name = "maxLatLabel";
+            this.maxLatLabel.Size = new System.Drawing.Size(35, 13);
+            this.maxLatLabel.TabIndex = 1;
+            this.maxLatLabel.Text = "label1";
+            // 
+            // minLatLabel
+            // 
+            this.minLatLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.minLatLabel.AutoSize = true;
+            this.minLatLabel.Location = new System.Drawing.Point(12, 431);
+            this.minLatLabel.Name = "minLatLabel";
+            this.minLatLabel.Size = new System.Drawing.Size(35, 13);
+            this.minLatLabel.TabIndex = 2;
+            this.minLatLabel.Text = "label2";
+            // 
+            // maxLonLabel
+            // 
+            this.maxLonLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.maxLonLabel.AutoSize = true;
+            this.maxLonLabel.Location = new System.Drawing.Point(562, 446);
+            this.maxLonLabel.Name = "maxLonLabel";
+            this.maxLonLabel.Size = new System.Drawing.Size(35, 13);
+            this.maxLonLabel.TabIndex = 4;
+            this.maxLonLabel.Text = "label4";
+            // 
+            // minLonLabel
+            // 
+            this.minLonLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.minLonLabel.AutoSize = true;
+            this.minLonLabel.Location = new System.Drawing.Point(77, 446);
+            this.minLonLabel.Name = "minLonLabel";
+            this.minLonLabel.Size = new System.Drawing.Size(35, 13);
+            this.minLonLabel.TabIndex = 5;
+            this.minLonLabel.Text = "label1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(176, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "60";
+            // 
+            // rangeTextBox
+            // 
+            this.rangeTextBox.Location = new System.Drawing.Point(414, 4);
+            this.rangeTextBox.Name = "rangeTextBox";
+            this.rangeTextBox.Size = new System.Drawing.Size(100, 20);
+            this.rangeTextBox.TabIndex = 7;
+            this.rangeTextBox.Text = "200";
+            // 
+            // angleTextBox
+            // 
+            this.angleTextBox.AutoSize = true;
+            this.angleTextBox.Location = new System.Drawing.Point(80, 7);
+            this.angleTextBox.Name = "angleTextBox";
+            this.angleTextBox.Size = new System.Drawing.Size(90, 13);
+            this.angleTextBox.TabIndex = 8;
+            this.angleTextBox.Text = "View Angle (deg):";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 352);
+            this.label2.Location = new System.Drawing.Point(318, 7);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Max Range (mm):";
             // 
-            // label3
+            // nextButton
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(77, 368);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "label3";
+            this.nextButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.nextButton.Cursor = System.Windows.Forms.Cursors.Default;
+            this.nextButton.Location = new System.Drawing.Point(201, 460);
+            this.nextButton.Name = "nextButton";
+            this.nextButton.Size = new System.Drawing.Size(75, 23);
+            this.nextButton.TabIndex = 10;
+            this.nextButton.Text = "Next";
+            this.nextButton.UseVisualStyleBackColor = true;
+            this.nextButton.Click += new System.EventHandler(this.nextButton_Click);
             // 
-            // label4
+            // autoButton
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(549, 368);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
-            this.label4.TabIndex = 4;
-            this.label4.Text = "label4";
+            this.autoButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.autoButton.Cursor = System.Windows.Forms.Cursors.Default;
+            this.autoButton.Location = new System.Drawing.Point(295, 460);
+            this.autoButton.Name = "autoButton";
+            this.autoButton.Size = new System.Drawing.Size(75, 23);
+            this.autoButton.TabIndex = 11;
+            this.autoButton.Text = "Auto";
+            this.autoButton.UseVisualStyleBackColor = true;
+            this.autoButton.Click += new System.EventHandler(this.autoButton_Click);
+            // 
+            // autoLabel
+            // 
+            this.autoLabel.AutoSize = true;
+            this.autoLabel.Location = new System.Drawing.Point(376, 465);
+            this.autoLabel.Name = "autoLabel";
+            this.autoLabel.Size = new System.Drawing.Size(13, 13);
+            this.autoLabel.TabIndex = 12;
+            this.autoLabel.Text = "0";
+            // 
+            // autoTimer
+            // 
+            this.autoTimer.Interval = 10;
+            this.autoTimer.Tick += new System.EventHandler(this.autoTimer_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.ClientSize = new System.Drawing.Size(643, 492);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
+            this.ClientSize = new System.Drawing.Size(657, 495);
+            this.Controls.Add(this.autoLabel);
+            this.Controls.Add(this.autoButton);
+            this.Controls.Add(this.nextButton);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.angleTextBox);
+            this.Controls.Add(this.rangeTextBox);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.minLonLabel);
+            this.Controls.Add(this.maxLonLabel);
+            this.Controls.Add(this.minLatLabel);
+            this.Controls.Add(this.maxLatLabel);
+            this.Controls.Add(this.pictureBox);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -102,11 +196,19 @@
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Label maxLatLabel;
+        private System.Windows.Forms.Label minLatLabel;
+        private System.Windows.Forms.Label maxLonLabel;
+        private System.Windows.Forms.Label minLonLabel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox rangeTextBox;
+        private System.Windows.Forms.Label angleTextBox;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button nextButton;
+        private System.Windows.Forms.Button autoButton;
+        private System.Windows.Forms.Label autoLabel;
+        private System.Windows.Forms.Timer autoTimer;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,10 +16,18 @@ namespace robotVizualizace
         string[] filepaths;
         double maxLon; double maxLat;
         double minLon; double minLat;
+        double degWidth; double degHeight;
+        double mmWidth; double mmHeight;
+        double pxLength; //in milimeters
+        long ptr = 0;
         public Form1(){
             InitializeComponent();
             ProcessFilenames();
-            FindMaxCoords();
+            FindMaxCoords();//finds measures
+            RescalePicturebox();
+            NextFile();
+
+            
         }
 
         private void ProcessFilenames(){
@@ -35,18 +43,73 @@ namespace robotVizualizace
             minLat = double.MaxValue;
             for (int i = 0; i < filepaths.Length; i++){
                 string s = getXMLvalue(filepaths[i], "lon");
-                double lon = double.Parse(s, System.Globalization.CultureInfo.InvariantCulture);
+                if(s == "") {continue;}
+                double lon = toDouble(s);
                 s = getXMLvalue(filepaths[i], "lat");
-                double lat = double.Parse(s, System.Globalization.CultureInfo.InvariantCulture);
+                if(s == "") {continue;}
+                double lat = toDouble(s);
 
                 maxLon = lon > maxLon ? lon : maxLon;
                 maxLat = lat > maxLat ? lat : maxLat;
                 minLon = lon < minLon ? lon : minLon;
                 minLat = lat < minLat ? lat : minLat;
             }
+            degWidth = maxLon-minLon;
+            degHeight = maxLat-minLat;
+            //figure all measures
+            mmHeight = 111120000 * degHeight;
+            mmWidth = 72731000 * degWidth; //at 49.3 degrees
+            pxLength = mmWidth / pictureBox.Width;
+        }
+        private void RescalePicturebox(){
+            minLonLabel.Text = minLon.ToString();
+            maxLonLabel.Text = maxLon.ToString();
+            minLatLabel.Text = minLat.ToString();
+            maxLatLabel.Text = maxLat.ToString();
+
+            double pbHeight = (double)pictureBox.Width * mmHeight / mmWidth;
+            pictureBox.Height = (int)Math.Round( pbHeight );
+            minLonLabel.Top = pictureBox.Top + pictureBox.Height;
+            maxLonLabel.Top = pictureBox.Top + pictureBox.Height;
+            minLatLabel.Top = pictureBox.Top + pictureBox.Height-minLatLabel.Height;
+
+            pictureBox.Image = new Bitmap(pictureBox.Width, pictureBox.Height);
+        }
+        int prevX; int prevY;
+        private void NextFile(){
+            string path = filepaths[ptr++];
+            string s = getXMLvalue(path, "lon");
+            if (s == "") { return; }
+            double lon = toDouble(s);
+            s = getXMLvalue(path, "lat");
+            if (s == "") { return; }
+            double lat = toDouble(s);
+
+            double xpos = pictureBox.Width * (lon-minLon) / degWidth;
+            double ypos = pictureBox.Height * (lat-minLat) / degHeight;
+            ypos = pictureBox.Height - ypos;
+
+            int x = (int)Math.Round(xpos);
+            int y = (int)Math.Round(ypos);
+
+            if (ptr != 1){
+                Graphics g = Graphics.FromImage(pictureBox.Image);
+                g.DrawLine(Pens.Blue,
+                        new Point(prevX, prevY),
+                        new Point(x, y)
+                    );
+                pictureBox.Refresh();
+            }
+            
+            prevX = x;
+            prevY = y;
+
+            autoLabel.Text = (ptr-1).ToString();
         }
 
-
+        private double toDouble(string from){
+            return double.Parse(from, System.Globalization.CultureInfo.InvariantCulture);
+        }
         public string getXMLvalue(string filepath, string label){
             StreamReader sr = new StreamReader(filepath);
             char c = '\0';
@@ -61,7 +124,7 @@ namespace robotVizualizace
                         s = s + c;
                         c = (char)sr.Read();
                     }
-                    if (s == "label"){
+                    if (s == label){
                         s = "";
                         c = (char)sr.Read();
                         while (c != '<'){
@@ -73,6 +136,24 @@ namespace robotVizualizace
                 }
             }
             return "";
+        }
+
+        private void nextButton_Click(object sender, EventArgs e){
+            if (ptr < filepaths.Length) {
+                NextFile();
+            }
+        }
+
+        private void autoButton_Click(object sender, EventArgs e){
+            autoTimer.Start();
+        }
+
+        private void autoTimer_Tick(object sender, EventArgs e){
+            if (ptr < filepaths.Length) {
+                NextFile();
+            } else {
+                autoTimer.Stop();
+            }
         }
     }
 }
